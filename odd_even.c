a43af28811946463ccf9c3f4003411c21d8eb06d
codered-here <uttkarshg2005@gmail.com>
2025-01-30 10:32:47 +0530

    Add files via upload

---
+++ odd_even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    int x;
+    printf("enter the number:");
+    scanf("%d",&x);
+    if(x%2==0){
+        printf("even");
+    }
+    else{
+        printf("odd");
+    }
+}
